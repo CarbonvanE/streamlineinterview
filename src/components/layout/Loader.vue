9fc97c931300eef557248d51753b01f413c42301
Ruben <25854734+CarbonvanE@users.noreply.github.com>
2019-09-18 16:25:27 +0200

    Create a generic Loader component

---
+++ src/components/layout/Loader.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="loader-wrapper">
+      <h3 v-if="!customers" class="title is-3">{{ text }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Loader',
+  props: {
+    text: {type: String, default: "Loading..."},
+  }
+}
+</script>
+
+<style>
+  .loader-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    margin: 50px;
+  }
+</style>
