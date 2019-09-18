b73d86173a5c64b49f9b1964b2d7757ffe4a6c60
Ruben <25854734+CarbonvanE@users.noreply.github.com>
2019-09-18 16:04:08 +0200

    Add vue-awesome to the main file so it can be used in all components

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from './router'
 import store from './store'
 
 import 'bulma/bulma.sass'
+import Icon from 'vue-awesome/icons'
 
 Vue.config.productionTip = false
+Vue.component('icon', Icon)
 
 new Vue({
   router,
