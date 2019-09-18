6f4ccd6529fda998279261540fd384b91d35fdba
Ruben <25854734+CarbonvanE@users.noreply.github.com>
2019-09-19 01:17:34 +0200

    Add a search bar to the customers page

---
+++ src/components/customers/SearchBar.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="search-bar-wrapper">
+    <div
+      v-if="query"
+      class="counter">
+        {{ count }} customer{{ count === 1 ? "" : "s" }} found
+    </div>
+    <div class="control has-icons-left">
+      <span class="icon is-small is-left">
+        <icon name="search"></icon>
+      </span>
+      <input
+        class="input is-rounded"
+        type="text"
+        placeholder="Find customer"
+        v-bind:value="query"
+        v-on:input="updateQuery($event.target.value)"
+      >
+    </div>
+  </div>
+</template>
+
+
+<script>
+  import Icon from 'vue-awesome/components/Icon'
+
+  export default {
+    name: 'SearchBar',
+    components: { Icon },
+    props: {
+      query: {type: String, required: true},
+      count: {type: Number, required: true}
+    },
+    methods: {
+      updateQuery: function (value) {
+        this.$emit('updateQuery', value);
+      }
+    }
+  }
+</script>
+
+
+<style scoped lang="scss">
+  @import "~bulma/sass/utilities/initial-variables";
+  @import "~bulma/sass/utilities/derived-variables";
+
+  .search-bar-wrapper {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    width: 100%;
+    margin-top: 30px;
+
+    .counter {
+      margin-right: 15px;
+      font-size: 0.9em;
+      font-weight: 600;
+      color: $grey-light;
+    }
+
+    .control {
+      width: 220px;
+      max-width: 100%;
+    }
+
+    @media only screen and (max-width: $tablet) {
+      flex-flow: column-reverse;
+      padding: 0 20px;
+
+      .counter {
+        margin-right: 0;
+        margin-top: 10px;
+      }
+
+      .control {
+        width: 100%;
+      }
+    }
+  }
+</style>
--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -1,22 +1,45 @@
 <template>
   <div class="customers">
+    <SearchBar v-if="customers" :query="query" @updateQuery="updateQuery" :count="this.filteredCustomers.length" />
     <Loader v-if="!customers" text="Loading customers..." />
-    <CustomersTable v-if="customers" :customers="customers" />
+    <CustomersTable v-if="customers" :customers="filteredCustomers" />
   </div>
 </template>
 
 <script>
   import CustomersTable from '@/components/customers/CustomersTable.vue'
+  import SearchBar from '@/components/customers/SearchBar.vue'
   import Loader from '@/components/layout/Loader.vue'
   import { fetchCustomers } from '@/utils/api'
 
   export default {
     name: 'customers',
-    components: { CustomersTable, Loader },
+    components: { CustomersTable, SearchBar, Loader },
 
     data () {
       return {
-        customers: null
+        customers: null,
+        query: ''
+      }
+    },
+
+    methods: {
+      updateQuery(variable) {
+        this.query = variable
+      }
+    },
+
+    computed: {
+      filteredCustomers: function() {
+        if (!this.query) return this.customers
+
+        const text = this.query.toLowerCase()
+        return this.customers.filter(customer => {
+          if (customer.name.toLowerCase().includes(text)) return true
+          if (customer.email.toLowerCase().includes(text)) return true
+          if (customer.address.toLowerCase().includes(text)) return true
+          else return false
+        })
       }
     },
 
