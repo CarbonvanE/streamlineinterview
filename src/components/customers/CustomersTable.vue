dd58161e301da5fba5b75447ed2ca2a3bfda483f
Ruben <25854734+CarbonvanE@users.noreply.github.com>
2019-09-19 00:51:28 +0200

    Make the table on the customers page scrollable in the horizontal direction

--- src/components/customers/CustomersTable.vue
+++ src/components/customers/CustomersTable.vue
@@ -1,39 +1,41 @@
 <template>
-  <table class="table is-hoverable">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Active</th>
-        <th><abbr title="Customer image">Img.</abbr></th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Phone</th>
-        <th>Address</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="customer in customers" v-bind:key="customer.id">
-        <th>{{ customer.id }}</th>
-        <td class="active"><icon :name="customer.isActive ? 'check' : 'times'"></icon></td>
-        <td class="customer-picture">
-          <img :src="customer.picture" :alt="`Picture of ${customer.picture}`">
-        </td>
-        <td>{{ customer.name }}</td>
-        <td>{{ customer.email }}</td>
-        <td>{{ customer.phone }}</td>
-        <td>{{ customer.address }}</td>
-        <td class="update">
-          <router-link
-            class="button is-outlined"
-            :to="`/customers/${customer.id}`"
-            tag="button">
-              Update
-          </router-link>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="table-container">
+    <table class="table is-hoverable">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Active</th>
+          <th><abbr title="Customer image">Img.</abbr></th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Address</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="customer in customers" v-bind:key="customer.id">
+          <th>{{ customer.id }}</th>
+          <td class="active"><icon :name="customer.isActive ? 'check' : 'times'"></icon></td>
+          <td class="customer-picture">
+            <img :src="customer.picture" :alt="`Picture of ${customer.picture}`">
+          </td>
+          <td>{{ customer.name }}</td>
+          <td>{{ customer.email }}</td>
+          <td>{{ customer.phone }}</td>
+          <td>{{ customer.address }}</td>
+          <td class="update">
+            <router-link
+              class="button is-outlined"
+              :to="`/customers/${customer.id}`"
+              tag="button">
+                Update
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 
