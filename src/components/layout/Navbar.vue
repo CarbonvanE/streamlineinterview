0e8d36f519cb81128c1c70abba59148254b0230c
Ruben <25854734+CarbonvanE@users.noreply.github.com>
2019-09-18 12:33:37 +0200

    Add a clock in the main navbar

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,15 +2,41 @@
     <nav class="navbar is-primary">
         <div class="navbar-brand">
             <a class="navbar-item" href="https://kiwaoesterbaai.com">
-                <div class="hero">Oesterbaai Interview</div>
+                <div class="hero">Customer database</div>
             </a>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-end">
+            <div class="datetime navbar-item">{{ datetime }}</div>
+          </div>
         </div>
     </nav>
 </template>
 
 <script>
-
-
 export default {
+  data () {
+    return {
+      datetime: new Date().toLocaleString()
+    }
+  },
+
+  methods: {
+    updateDatetime: function() {
+      window.setInterval(() => {
+        this.datetime = new Date().toLocaleString()
+      }, 1000)
+    }
+  },
+
+  mounted () {
+    this.updateDatetime();
+  }
 }
 </script>
+
+<style>
+  .datetime {
+    opacity: 0.65;
+  }
+</style>
