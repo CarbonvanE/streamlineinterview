2b137377504cb2aaa37ae5d8d75971cf4a8619b8
Ruben <25854734+CarbonvanE@users.noreply.github.com>
2019-09-19 18:25:46 +0200

    Create a custom Button component

---
+++ src/components/layout/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="button"
+    :class="{
+      [`is-${type}`]: true,
+      'is-loading': loading,
+      'is-fullwidth': fullWidth,
+    }"
+    v-on:click.prevent="onClick">
+      {{ text }}
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+    props: {
+      text: {type: String, required: true},
+      onClick: {type: Function, required: true},
+      type: {type: String, required: false, default: 'link'},
+      loading: {type: Boolean, required: false},
+      fullWidth: {type: Boolean, required: false},
+    }
+  }
+</script>
