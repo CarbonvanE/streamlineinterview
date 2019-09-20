da37b7101203a6a36d76b11f3a1971f8addfe667
Ruben <25854734+CarbonvanE@users.noreply.github.com>
2019-09-20 12:57:09 +0200

    Create the NotFound404 component

---
+++ src/components/error/NotFound404.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="not-found-404">
+    <icon name="exclamation-triangle" />
+    <h3 class="title is-3" v-if="!customerExists">
+      Customer '{{ this.$route.params.id }}' doesn't exist
+    </h3>
+  </div>
+</template>
+
+<script>
+  import Icon from 'vue-awesome/components/Icon'
+
+  export default {
+    name: 'NotFound404',
+    components: { Icon },
+    props: {
+      message: {type: String, required: false, default: '404 Page not Found'},
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .not-found-404 {
+    margin: 50px;
+    width: 100%;
+    text-align: center;
+
+    svg.fa-icon {
+      // opacity: 0.67;
+      height: 4em;
+      width: 4em;
+      margin-bottom: 2em;
+    }
+  }
+</style>
