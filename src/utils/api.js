f46a3926c7d860677dc64bc74ef6710bec76b551
Ruben <25854734+CarbonvanE@users.noreply.github.com>
2019-09-23 10:43:05 +0200

    Update the general API function so that it can also handle POST, PUT, and DELETE requests

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,16 @@
 import store from '../store'
 
 // Return the JSON data from the server given certain parameters
-function fetchData(params) {
-  const url = 'https://jsonbox.io/vqyzQKG36ZFyi3bvxPJqrepGV27gMCTU' + params
+function customerAPI(params, method = "GET", body = null) {
   return new Promise(async function(resolve, reject) {
     try {
-      const response = await fetch(url)
-      const data = await response.json()
-      resolve(data)
+      const url = 'https://jsonbox.io/vqyzQKG36ZFyi3bvxPJqrepGV27gMCTU' + params
+      const headers = { 'Accept': 'application/json', 'Content-Type': 'application/json' }
+      const response = await fetch(url, { method , headers, body })
+      resolve({
+        status: response.status,
+        data: await response.json()
+      })
     }
     catch {
       reject()
@@ -17,49 +20,38 @@ function fetchData(params) {
 
 // Return all customers
 export async function fetchCustomers() {
-  const customers = await fetchData('?sort=id&limit=500')
-  store.commit('setCustomers', customers)
-  return customers
+  const { data } = await customerAPI('?sort=id&limit=500')
+  store.commit('setCustomers', data)
+  return data
 }
 
 // Update a single customer
 export async function updateCustomer(_id, data) {
-  const response = await fetch(`https://jsonbox.io/vqyzQKG36ZFyi3bvxPJqrepGV27gMCTU/${_id}`, {
-    method: "PUT",
-    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  })
-  if (response.status !== 200) return false
+  const { status } = await customerAPI(`/${_id}`, "PUT", JSON.stringify(data))
+  if (status !== 200) return false
   store.commit('updateCustomer', { _id, data })
   return true
 }
 
 // Delete a single customer
 export async function deleteCustomer(_id) {
-  const response = await fetch(`https://jsonbox.io/vqyzQKG36ZFyi3bvxPJqrepGV27gMCTU/${_id}`, {
-    method: "DELETE",
-    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-  })
-  if (response.status !== 200) return false
+  const { status } = await customerAPI(`/${_id}`, "DELETE")
+  if (status !== 200) return false
   store.commit('deleteCustomer', _id)
   return true
 }
 
 // Return the current highest ID
 export async function fetchLastId() {
-  const data = await fetchData('?sort=-id&limit=1')
+  const { data } = await customerAPI('?sort=-id&limit=1')
   if (Array.isArray(data)) return data[0].id
   return null
 }
 
 // Save a new customer
-export async function createCustomer(data) {
-  const response = await fetch('https://jsonbox.io/vqyzQKG36ZFyi3bvxPJqrepGV27gMCTU', {
-    method: "POST",
-    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  })
-  if (response.status !== 200) return false
-  store.commit('createCustomer', await response.json())
+export async function createCustomer(newCustomer) {
+  const { status, data } = await customerAPI('', 'POST', JSON.stringify(newCustomer))
+  if (status !== 200) return false
+  store.commit('createCustomer', data)
   return true
 }
